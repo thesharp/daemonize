9582aca78c281dccbccecd1b6336589e33aa77c2
Ilya A. Otyutskiy <sharp@thesharp.ru>
2012-09-01 01:25:38 +0400

    Fix README and add LICENSE to MANIFEST.in.

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "daemonize",
-    version = "1.3",
-    py_modules = ['daemonize'],
-    author = "Ilya A. Otyutskiy",
-    author_email = "sharp@thesharp.ru",
-    maintainer = "Ilya A. Otyutskiy",
-    url = "https://github.com/thesharp/daemonize",
-    description = "Library to enable your code run as a daemon process "
-                  "on Unix-like systems.",
-    license = "PSF"
+    name="daemonize",
+    version="1.3",
+    py_modules=["daemonize"],
+    author="Ilya A. Otyutskiy",
+    author_email="sharp@thesharp.ru",
+    maintainer="Ilya A. Otyutskiy",
+    url="https://github.com/thesharp/daemonize",
+    description="Library to enable your code run as a daemon process "
+                "on Unix-like systems.",
+    license="PSF"
 )
