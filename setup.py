e28ba2857a89ca5f8e11625639e1bb06ce741249
Ilya Otyutskiy <ilya.otyutskiy@icloud.com>
2013-03-30 02:08:06 +0400

    New version: 2.2.
    - Add classifiers to setup.py.
    - Drop Python 2.5 support.
    - Add Python 3.3. support.
    - Reformat comments to 100 chars word wrap.

--- setup.py
+++ setup.py
@@ -4,13 +4,23 @@ from setuptools import setup, find_packages
 
 setup(
     name="daemonize",
-    version="2.1.1",
+    version="2.2",
     py_modules=["daemonize"],
-    author="Ilya A. Otyutskiy",
-    author_email="sharp@thesharp.ru",
-    maintainer="Ilya A. Otyutskiy",
+    author="Ilya Otyutskiy",
+    author_email="ilya.otyutskiy@icloud.com",
+    maintainer="Ilya Otyutskiy",
     url="https://github.com/thesharp/daemonize",
-    description="Library to enable your code run as a daemon process "
-                "on Unix-like systems.",
-    license="MIT"
+    description="Library to enable your code run as a daemon process on Unix-like systems.",
+    license="MIT",
+    classifiers=["Development Status :: 5 - Production/Stable",
+                 "Environment :: Console",
+                 "Intended Audience :: Developers",
+                 "License :: OSI Approved :: MIT License",
+                 "Operating System :: MacOS :: MacOS X",
+                 "Operating System :: POSIX :: Linux",
+                 "Programming Language :: Python",
+                 "Programming Language :: Python :: 2.6",
+                 "Programming Language :: Python :: 2.7",
+                 "Programming Language :: Python :: 3.3",
+                 "Topic :: Software Development"]
 )
