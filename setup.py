86ed4f046d2cf0e583c1446ce95fc8b4e733bd33
Ilya Otyutskiy <ilya.otyutskiy@icloud.com>
2013-03-30 17:44:59 +0400

    Fix classifiers.

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
                  "Programming Language :: Python",
                  "Programming Language :: Python :: 2.6",
                  "Programming Language :: Python :: 2.7",
+                 "Programming Language :: Python :: 3",
                  "Programming Language :: Python :: 3.3",
                  "Topic :: Software Development"]
 )
